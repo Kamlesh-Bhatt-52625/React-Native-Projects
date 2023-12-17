4f2d95b9b0cc73efb82613336e8ddfa64f81f531
Kamlesh Bhatt <kb55262555@gmail.com>
2023-12-17 20:24:04 +0530

    Installed Yup and used it for validation

--- PasswordGenerator/App.tsx
+++ PasswordGenerator/App.tsx
@@ -1,6 +1,16 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
+// Form Validation
+import * as Yup from 'yup';
+
+const PasswordSchema = Yup.object().shape({
+  passwordLength: Yup.number()
+    .min(4, 'Should be minimum of 4 characters!')
+    .max(16, 'Should be maximum of 16 characters')
+    .required('Length is required'),
+});
+
 export default function App() {
   return (
     <View>
